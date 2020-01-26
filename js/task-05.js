4020689cb88e3df3fe199f4be6744bb727f7030f
ellie25v <yelizarieva.ellie@gmail.com>
2020-01-26 16:53:14 +0200

    e

--- js/task-05.js
+++ js/task-05.js
@@ -4,24 +4,53 @@
 //     return arr.map(el => el[prop]);
 // };
 
-const getAllPropValues = function(arr, prop) {
-    const newArr = [];
-    for (const item of arr) {
-        let value = item[prop];
+// const getAllPropValues = function(arr, prop) {
+//     const newArr = [];
+//     for (const item of arr) {
+//         let value = item[prop];
 
-        if (value !== undefined) {
-            newArr.push(value);
+//         if (value !== undefined) {
+//             newArr.push(value);
+//         }
+//     }
+//     return newArr;
+// }
+
+const getAllPropValues = function (arr, prop) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const entries = Object.entries(arr[i]);
+
+        for (const item of entries) {
+            if (item.includes(prop)) {
+                newArr.push(item[1]);
+            }
         }
     }
     return newArr;
 }
 
 
-const products = [
-  { name: 'Радар', price: 1300, quantity: 4 },
-  { name: 'Сканер', price: 2700, quantity: 3 },
-  { name: 'Дроид', price: 400, quantity: 7 },
-  { name: 'Захват', price: 1200, quantity: 2 },
+const products = [{
+        name: 'Радар',
+        price: 1300,
+        quantity: 4
+    },
+    {
+        name: 'Сканер',
+        price: 2700,
+        quantity: 3
+    },
+    {
+        name: 'Дроид',
+        price: 400,
+        quantity: 7
+    },
+    {
+        name: 'Захват',
+        price: 1200,
+        quantity: 2
+    },
 ];
 
 
